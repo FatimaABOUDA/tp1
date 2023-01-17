f33db35f4cccc9dffe680cd9dc78c03876beace3
ABOUDA <abouda.fatima@etu.educ-valadon-limoges.fr>
2023-01-17 15:39:11 +0100

    ajout de la classe main

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) { float prix1,prix2,prix3,total;
+        /*exercice 2
+        prix1=11.5f;
+        prix2=8.5f;
+        prix3 = 4f;
+        total = prix1+prix2+prix3;
+        System.out.println(total);
+exercice 1
+        System.out.println("Hello world!");
+        exercice3
+        double nombre = 3.14;
+        System.out.println(nombre);
+        int entier;
+        entier = (int)nombre;
+        System.out.println(entier);
+         Exercice4
+        Scanner sc = new Scanner(System.in);
+        System.out.println("saisissez votre prenom");
+        String prenom = sc.next();
+        System.out.println("saisissez votre nom");
+        String nom = sc.next();
+        System.out.println("Bonjour "+prenom+" "+nom);
+
+         */
+
+    }
+}
